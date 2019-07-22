d8661c9a1890d1854b82030cfd21facc386a93cd
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-07-22 17:08:59 -0400

    Controller Inicio da aplicação

--- Startup.cs
+++ Startup.cs
@@ -50,7 +50,7 @@ namespace FaceAlbum
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Albuns}/{action=Index}/{id?}");
             });
         }
     }
